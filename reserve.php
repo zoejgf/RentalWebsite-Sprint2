c19ae0b5217b06a1d7e6d8eec7aaee3c1a92315e
shackerman <jec.alferez02@gmai.com>
2022-10-20 12:10:39 -0700

    changed set to option

--- reserve.php
+++ reserve.php
@@ -5,19 +5,19 @@
  <body>
  <?php 
     echo '<p>Your data is:</p>'; 
-    if($_GET['set'] == "1")
+    if($_GET['option'] == "1")
     {
         echo '<p>Layered Arch</p>';
     }
-    elseif($_GET['set'] == "2")
+    elseif($_GET['option'] == "2")
     {
         echo "<p>Modern Round</p>";
     }
-    elseif($_GET['set'] == "3")
+    elseif($_GET['option'] == "3")
     {
         echo "<p>Vintage Mirrors</p>";
     }
-    elseif($_GET['set'] == "4")
+    elseif($_GET['option'] == "4")
     {
         echo "<p>Dark Walnut</p>";
     }
