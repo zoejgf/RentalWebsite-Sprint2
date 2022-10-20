7505159990d6aac4ed84447bc92acf7114135549
j <jec.alferez02@gmail.com>
2022-10-20 11:17:25 -0700

    added reserve+vardump.php

---
+++ vardump.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>vardump</title>
+ </head>
+ <body>
+  <?php echo '<p>POST</p>'; ?> 
+  <?php echo var_dump($_POST); ?>
+
+  <?php echo '<p>GET</p>'; ?> 
+  <?php echo var_dump($_GET); ?> 
+ </body>
+</html>
