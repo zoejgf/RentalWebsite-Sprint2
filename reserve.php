8b0d2a6cd3e644215495453fbb3e2984f672d847
shackerman <jec.alferez02@gmai.com>
2022-10-20 12:04:59 -0700

    added light content to reserve.php - will likely need update once other phps are made.

--- reserve.php
+++ reserve.php
@@ -3,10 +3,31 @@
   <title>reserve</title>
  </head>
  <body>
-  <?php echo '<p>POST</p>'; ?> 
-  <?php echo var_dump($_POST); ?>
-
-  <?php echo '<p>GET</p>'; ?> 
-  <?php echo var_dump($_GET); ?> 
+ <?php 
+    echo '<p>Your data is:</p>'; 
+    if($_GET['set'] == "1")
+    {
+        echo '<p>Layered Arch</p>';
+    }
+    elseif($_GET['set'] == "2")
+    {
+        echo "<p>Modern Round</p>";
+    }
+    elseif($_GET['set'] == "3")
+    {
+        echo "<p>Vintage Mirrors</p>";
+    }
+    elseif($_GET['set'] == "4")
+    {
+        echo "<p>Dark Walnut</p>";
+    }
+    else
+    {
+        echo '<p>Rustic Wood</p>';
+    }
+    
+    echo $_GET[date];
+    echo $_GET[subpackage];
+    echo $_GET[delivery];
  </body>
 </html>
