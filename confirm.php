8271c5dfcab160b3e366bd28c61bf87a471cef96
shackerman <jec.alferez02@gmai.com>
2022-10-27 11:34:33 -0700

    working email thingie

--- confirm.php
+++ confirm.php
@@ -262,7 +262,24 @@
                 3 - Pick 4 199
 
 */
+$to = "Alferez-Ruiz.Jeconiah@student.greenriver.edu";
+$subject = "HTML email";
 
+$message = "<!DOCTYPE html>
+<body> boobs
+   
+    </body>
+</html>";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <webmaster@example.com>' . "\r\n";
+$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+mail($to,$subject,$message,$headers);
 
 ?>
 
