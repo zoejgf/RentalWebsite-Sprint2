0e00009571c203d91e932951cdd57eeedde8571e
shackerman <jec.alferez02@gmai.com>
2022-11-10 13:20:52 -0800

    made admin page work with pauls db-access.php file

--- admin.php
+++ admin.php
@@ -1,12 +1,25 @@
 <?php
 
-$username = 'admin';
-$password = 'admin';
-$hostname = 'localhost';
-$database = 'redgreen_database';
+require "db-access.php";
+$results = queryReservations();
 
-$cnxn = @mysqli_connect($hostname, $username, $password, $database) or die("Error Connecting to DB: " . mysqli_connect_error());
+while ($row = mysqli_fetch_assoc($result)) {
+    $reservationID = $row['reservation_id'];
+    $set = $row['set'];
+    $package = $row['package'];
+    $date = $row['date'];
+    $customerID = $row['customer_id'];
+    $first = $row['fname'];
+    $last = $row['lname'];
+    $phone = $row['phone'];
+    $email = $row['email'];
+    
+    echo "<p>$reservationID, $set, $package, $date, $customerID, $first, $last, $phone, $email</p>";
+}
 
-echo 'connected to Database!';
+
+
+
+
 
 ?>
